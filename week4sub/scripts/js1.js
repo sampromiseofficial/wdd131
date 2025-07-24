67c172d8bbfb785fae8a62882d375bfdcbe5cea5
sampromiseofficial <sampromiseofficial@gmail.com>
2025-07-24 11:32:57 +0100

    G2G

---
+++ week4sub/scripts/js1.js
@@ -0,0 +1,20 @@
+let aCourse = {
+  code: "WDD131",
+  title: "Dynamic Web Fundamentals",
+  credits: 2,
+};
+
+function setCourseInformation(course) {
+  
+}
+
+function sectionTemplate(section) {
+  
+}
+
+function renderSections(sections) {
+  
+}
+
+setCourseInformation(aCourse);
+renderSections(aCourse.sections);
